6fc33def089ad6f7ea8a62365193c63eb0b0ae34
Koval <dmytrokovalskyi@yahoo.com>
2020-09-08 16:46:36 +0300

    task 2 commit

---
+++ build/precache-manifest.a97fc78b7934a3dcc686f279e3ae6fa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b636f812507b4b7e6e4e32f6ce8913",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "507a412aa293250bc706",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "8bbcd465518f9b51c8d7",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.59fd2137.chunk.css"
+  },
+  {
+    "revision": "507a412aa293250bc706",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c4bd7c6c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c4bd7c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbcd465518f9b51c8d7",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.3c14efa0.chunk.js"
+  },
+  {
+    "revision": "55581d768912a9cdd1d4",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3a4847fc.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component}from 'react';
+// import { v4 as uuidv4 } from "uuid";
+import Form from "./components/Form/Form"
+import ContactList from "./components/contactList/ContactList"
+import Filter from "./components/Filter/Filter"
+import styles from "./components/global.module.css"
+
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+  
+  getContact = (addedContact) => {
+    this.state.contacts.some((el) => el.name === addedContact.name)
+      ? alert(`${addedContact.name} is already in contacts `)
+      : this.setState((prevState) => {
+          return {
+            contacts: [...prevState.contacts, addedContact],
+          };
+        });
+  };
+  handleChange = event => {
+    this.setState({ name: event.target.value })
+  };
+
+  getFilteredName = event => {
+   this.setState({ filter: event.target.value})
+  };
+
+  deleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((el) => el.id !== id),
+    }));
+  };
+
+  getFilteredContacts = () => {
+    return this.state.filter
+      ? this.state.contacts.filter(el => 
+        el.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    
+      )
+      : this.state.contacts;
+  }
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <h1>Phonebook</h1>
+        <Form getContact={this.getContact} />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} getFilterName={this.getFilteredName} >
+        </Filter>
+        <ContactList contactList={this.getFilteredContacts()}
+          deleteContact={this.deleteContact}>
+          </ContactList>
+      </div>
+    );
+  }
+};
+
+export default App;
